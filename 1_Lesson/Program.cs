91ac1c3bb3ae4034c2026bbbe3aa62aedfcc2364
Ekaterina <makrusova96@mail.ru>
2023-02-23 13:58:01 +0300

    Task_1_1

---
+++ 1_Lesson/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача №0. Напишите программу, которая  на вход принимает число и выдает его квадрат.
+// Например:
+// 4 -> 16; -3-> 9;-7 -> 49
+
+System.Console.WriteLine("Введите число: ");
+// string s = Console.ReadLine()!;
+int num = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(num * num);
