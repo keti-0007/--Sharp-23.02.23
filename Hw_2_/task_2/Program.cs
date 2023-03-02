85f174a34453b342d449f4f9120a86309a0b412b
Ekaterina <makrusova96@mail.ru>
2023-03-02 21:22:52 +0300

    task_4

--- Hw_2_/task_2/Program.cs
+++ Hw_2_/task_2/Program.cs
@@ -4,17 +4,44 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
+Console.Clear();
+System.Console.WriteLine("Введите число: ");
+
 void ThreNum (int a)
 {
-    while(a > 1000)
-    a = a/10 + a%100;
-    if (a < 100)
+    if(a < 100)
     {
-        System.Console.WriteLine($"Третьей цифры нет {a}");
-
+        System.Console.WriteLine($"Третьей цифры нет");
+        return;
     }
+
+    while(a > 999)
+    a = a/10;
+    System.Console.WriteLine($"Третья цифра данного числа: {a%10}");
+    
+   
 }
 
-ThreNum();
+ThreNum(int.Parse(Console.ReadLine()!));
 
 
+//II вариант
+
+// Console.Clear();
+// Console.Write("Введите число: ");
+// int number = int.Parse(Console.ReadLine()!);
+
+// if (Math.Abs(number) > 100)
+// {
+//     while (Math.Abs(number) >999)
+//     {
+//         number = number / 10;
+
+//     }
+//     number = Math.Abs(number % 10); 
+//     System.Console.WriteLine($"Третья цифра {number}");
+// }
+// else
+// {
+//     System.Console.WriteLine("Третьей цифры нет");
+// }
---
+++ Hw_2_/task_4/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+
+Console.Write("Введите номер(от 1 до 7) дня недели:");
+ int day = Convert.ToInt32(Console.ReadLine());
+
+ while (day < 1 || day > 7);
+
+switch (day) 
+{
+case 6:
+case 7:
+Console.WriteLine("Выходной");
+break;
+
+default:
+Console.WriteLine("Рабочий день");
+break;
+}
+
+//II Вариант
+// Console.Clear();
+// Console.Write("Введите номер(от 1 до 7) дня недели: ");
+
+//  void DayOff(int a)
+//  {
+//     string text = " Не соответсвует номеру дня недели, повторитте ввод!";
+
+//     if (a == 7 || a == 6) text = "Выходной день ";
+//     else if (a < 6 && a > 0 ) text = "Рабочий день ";
+    
+// System.Console.WriteLine($"{a} {text}");
+//  }
+//   DayOff(int.Parse(Console.ReadLine()!));
