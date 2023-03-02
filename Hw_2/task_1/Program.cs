051e067c3530299796b6dc32a009859b4b105f98
Ekaterina <makrusova96@mail.ru>
2023-03-02 18:02:15 +0300

    task_1

---
+++ Hw_2/task_1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//не add . -->commit -m --> push
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+Console.Clear();
+System.Console.WriteLine("Введите трехзначное число: ");
+int num = int.Parse(Console.ReadLine()!);
+
+int SearchNum(int num)
+{
+        return num %100/10;
+}
+int result = SearchNum(num);
+System.Console.WriteLine(result);
