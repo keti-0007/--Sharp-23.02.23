af76119f2d1da307521758dcb6115168bf49f045
Ekaterina <makrusova96@mail.ru>
2023-03-06 11:22:57 +0300

    4_2,3

---
+++ 4_Lesson/4_2/Program.cs
@@ -0,0 +1,18 @@
+﻿//// 2. Напишите программу, которая принимает на вход
+//    число N и выдаёт произведение чисел от 1 до N.
+
+
+int CompositionNum(int num)
+{
+    int mutiply = 1;
+    for(int i = 1; num >= i; i++)
+        mutiply *= i;
+
+    return mutiply;
+}
+System.Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+int result = CompositionNum(N);
+Console.WriteLine(result);
+
+
