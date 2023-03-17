138b3c40d05cce826a43d1647d34bcfa2f42e7dc
Ekaterina <makrusova96@mail.ru>
2023-03-17 20:53:33 +0300

    7_2

---
+++ 7_Lesson/7_2/Program.cs
@@ -0,0 +1,54 @@
+﻿// 2. Задайте двумерный массив. Найдите элементы,
+//    у которых обе позиции чётные, и замените эти элементы на их квадраты.
+ 
+
+void Print(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)        
+            Console.Write($" {arr[i, j]} ");        
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)    
+        for (int j = 0; j < column; j++)        
+            arr[i, j] = new Random().Next(from, to);        
+    
+    return arr;
+}
+
+void FindTwoEven(int[,] arr)
+{
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+
+    for (int i = 1; i < row; i += 2)    
+        for (int j = 1; j < column; j += 2)        
+            arr[i, j] *= arr[i, j];   
+}
+
+
+Console.Write("Enter the number of rows: ");
+int row_num = int.Parse(Console.ReadLine()!);
+Console.Write("Enter the number of columns: ");
+int column_num = int.Parse(Console.ReadLine()!);
+
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+
+int[,] mass = MassNums(row_num, column_num, start, stop);
+
+Print(mass);
+FindTwoEven(mass);
+Console.WriteLine();
+Print(mass);
