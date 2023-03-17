3b832b3a5022b2502dce071141f47e558e43093f
Ekaterina <makrusova96@mail.ru>
2023-03-17 20:34:30 +0300

    7_0

--- 5_Lesson/Hw_5_3/Program.cs
+++ 5_Lesson/Hw_5_3/Program.cs
@@ -2,47 +2,100 @@
 // [3 7 22 2 78] -> 76
 
 
+// void Print(double[] arr)
+// {
+//     int size = arr.Length;
+
+//     for (int i = 0; i < size; i++)
+//         Console.Write($"{arr[i]} ");
+//     Console.WriteLine();
+// }
+
+// double[] MassNums(int size, int from, int to)
+// {
+//        double[] arr = new double[size];
+//        Random n_new = new Random();
+
+
+//     for (int i = 0; i < size; i++)
+//     arr[i] = Math.Round(n_new.NextDouble() * (from + to) - from, 2);
+//     return arr;
+// }
+
+// double MassMaxMin(int a, int b)
+// {
+//     Random rnd = new Random();
+//     double rndDouble = a + rnd.NextDouble()*(b-a);
+//   return rndDouble;
+// }
+
+
+// Console.WriteLine("Введите длину массива: ");
+// int num = int.Parse(Console.ReadLine()!);
+
+// Console.WriteLine("Введите минимальное значение для диапозона случайного числа: ");
+// int start = int.Parse(Console.ReadLine()!);
+
+// Console.WriteLine("Введите максимальное значение для диапозона случайного числа: ");
+// int stop = int.Parse(Console.ReadLine()!);
+
+// double[] mass = MassNums(num, start, stop);
+// Print(mass);
+
+// Console.WriteLine($"Max {mass.Max()} Min {mass.Min()} Разница max-min = {mass.Max()-mass.Min()}");
+
+
+// 3. Задайте массив вещественных чисел. 
+//    Найдите разницу между максимальным и минимальным элементов массива. 
+//    https://docs.microsoft.com/en-us/dotnet/api/system.random.nextdouble?view=net-6.0
+//    https://ru.stackoverflow.com/questions/1254105/%D0%9A%D0%B0%D0%BA-%D1%81%D0%B3%D0%B5%D0%BD%D0%B5%D1%80%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D1%82%D1%8C-%D1%81%D0%BB%D1%83%D1%87%D0%B0%D0%B9%D0%BD%D0%BE%D0%B5-%D0%B4%D1%80%D0%BE%D0%B1%D0%BD%D0%BE%D0%B5-%D1%87%D0%B8%D1%81%D0%BB%D0%BE-%D0%B8%D0%B7-%D0%B8%D0%BD%D1%82%D0%B5%D1%80%D0%B2%D0%B0%D0%BB%D0%B0
+
+
 void Print(double[] arr)
 {
     int size = arr.Length;
 
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i < size; i++)    
         Console.Write($"{arr[i]} ");
+    
     Console.WriteLine();
 }
 
 double[] MassNums(int size, int from, int to)
 {
     double[] arr = new double[size];
+    Random n_new = new Random();
 
-    for (int i = 0; i < size; i++)
-        arr[i] = new Random().Next(from, to);
+    for (int i = 1; i < size; i++)    
+        arr[i] = Math.Round(n_new.NextDouble() * (from + to) - from, 2);
+    
     return arr;
 }
 
-double MassMaxMin(int a, int b)
+void DiffMaxMin(double[] arr)
 {
-    Random rnd = new Random();
-    double rndDouble = a + rnd.NextDouble()*(b-a);
-  return rndDouble;
+    double n_max = arr[0];
+    double n_min = arr[0];
+
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if (n_max < arr[i])        
+            n_max = arr[i];
+        
+        else if (n_min > arr[i])        
+            n_min = arr[i];        
+    }
+
+    Console.Write($"Max: {n_max}, min: {n_min}. ");
+    Console.WriteLine($"Difference: {n_max} - ({n_min}) = {Math.Round(n_max - n_min, 2)}");
 }
 
-
-Console.WriteLine("Введите длину массива: ");
-int num = int.Parse(Console.ReadLine()!);
-
-Console.WriteLine("Введите минимальное значение для диапозона случайного числа: ");
-int start = int.Parse(Console.ReadLine()!);
-
-Console.WriteLine("Введите максимальное значение для диапозона случайного числа: ");
-int stop = int.Parse(Console.ReadLine()!);
-
-double[] mass = MassNums(num, start, stop);
-Print(mass);
-
-Console.WriteLine($"Max {mass.Max()} Min {mass.Min()} Разница max-min = {mass.Max()-mass.Min()}");
-
-
+// 10, 10, 12
+double[] arr_1 = MassNums(int.Parse(Console.ReadLine()),
+                          int.Parse(Console.ReadLine()),
+                          int.Parse(Console.ReadLine()));
+Print(arr_1);
+DiffMaxMin(arr_1);
 
 
 
---
+++ 7_Lesson/7_0/Program.cs
@@ -0,0 +1,42 @@
+﻿// 0. Задайте двумерный массив размером m×n, 
+//    заполненный случайными целыми числами. 
+
+
+void Print(int[,] arr)
+{
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+            Console.Write($" {arr[i, j]} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(from, to);
+    return arr;
+}
+Console.WriteLine("Введите количество строк: ");
+int row_num = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите количество столбцов ");
+int column_num = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите минимальное значение для диапозона случайного числа: ");
+int start = int.Parse(Console.ReadLine()!);
+
+int stop = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите максимальное значение для диапозона случайного числа: ");
+
+int[,] mass = MassNums(row_num, column_num, start, stop);
+Print(mass);
+
