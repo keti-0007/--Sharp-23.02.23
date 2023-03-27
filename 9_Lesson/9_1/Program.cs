af4138c988e84d654327898c72db6f2261945284
Ekaterina <makrusova96@mail.ru>
2023-03-27 12:00:39 +0300

    9_1

---
+++ 9_Lesson/9_1/Program.cs
@@ -0,0 +1,13 @@
+﻿// 1. Задайте значение N. Напишите программу, которая выведет
+//    все натуральные числа в промежутке от 1 до N при помощи рекурсии.
+
+void SeriesNum(int N)
+{
+    if (N == 0) return;
+    SeriesNum(N - 1);// вызываем рекурсию (метод). Он движется к 0, поэтому N -1 
+    System.Console.Write($"{N}, ");
+}
+System.Console.Write("Введите число: ");
+int Num = int.Parse(Console.ReadLine()!);
+
+SeriesNum(Num);
