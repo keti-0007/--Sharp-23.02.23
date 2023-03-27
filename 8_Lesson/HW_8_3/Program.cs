e845503c16386b6c09e38320dd9f00116396b649
Ekaterina <makrusova96@mail.ru>
2023-03-27 10:07:01 +0300

    HW_8_4_

---
+++ 8_Lesson/HW_8_3/Program.cs
@@ -0,0 +1,96 @@
+﻿
+
+//Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+//    https://blog.fenix.help/podgotovka-k-testam-yekzamenam-zachetam/pravilo-umnozheniya-matrits-primery-s-resheniyem
+//    https://lfirmal.com/matritsyi-chastnyie-vidyi-matrits/#%D0%A3%D0%BC%D0%BD%D0%BE%D0%B6%D0%B5%D0%BD%D0%B8%D0%B5_%D0%BC%D0%B0%D1%82%D1%80%D0%B8%D1%86
+//    https://ru.onlinemschool.com/math/assistance/matrix/multiply
+
+
+
+void Print(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+            Console.Write($" {arr[i, j],3} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(from, to + 1);
+    return arr;
+}
+
+int[,] MatrixProduct(int[,] arrayFirst, int[,] arraySecond)
+{
+    int row_size_1 = arrayFirst.GetLength(0);
+    int column_size_1 = arrayFirst.GetLength(1);
+
+    int row_saiz_2 = arraySecond.GetLength(0);
+    int column_size_2 = arraySecond.GetLength(1);
+
+
+    int[,] pr_matrix = new int[row_size_1, column_size_1];
+
+    if (column_size_1 != row_saiz_2) return pr_matrix;//если  количество столбцов  первой матрицы  не равно количеству строк второй матрицы,такие матрицы нельзя перемножить 
+
+    //Для того, чтобы умножить 1-ю и 2-ю, нужно взять строку в первой матрице и столбец во второй,
+    // перемножить одинаковые элементы и сложить полученные произведения.
+
+    else if (column_size_1 == row_saiz_2)
+    {
+        pr_matrix = new int[row_size_1, column_size_2];
+
+        for (int i = 0; i < row_size_1; i++)
+        {
+            for (int j = 0; j < column_size_2; j++)
+            {
+                for (int g = 0; g < column_size_1; g++)
+
+                    pr_matrix[i, j] += arrayFirst[i, g] * arraySecond[g, j];
+            }
+        }
+    }
+    return pr_matrix;
+
+}
+
+
+Console.Write("Введите количество строк первой матрицы: ");
+int row_1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов первой матрицы: ");
+int column_1 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите количество строк второй матрицы: ");
+int row_2 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов второй матрицы: ");
+int column_2 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите минимальное значение для диапозона случайного числа для первой матрицы: ");
+int start_1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите максимальное значение для диапозона случайного числа для первой матрицы: ");
+int stop_1 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите минимальное значение для диапозона случайного числа для второй матрицы: ");
+int start_2 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите максимальное значение для диапозона случайного числа для второй матрицы: ");
+int stop_2 = int.Parse(Console.ReadLine()!);
+
+int[,] arr_1 = MassNums(row_1, column_1, start_1, stop_1);
+Print(arr_1);
+
+int[,] arr_2 = MassNums(row_2, column_2, start_2, stop_2);
+Print(arr_2);
+
+int[,] res_matrix = MatrixProduct(arr_1, arr_2);
+Print(res_matrix);
---
+++ 8_Lesson/Hw_8_4/Program.cs
@@ -0,0 +1,95 @@
+﻿
+// Задача 59: Задайте двумерный массив из целых чисел. Напишите программу, которая удалит строку и столбец, на пересечении которых расположен наименьший элемент массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Наименьший элемент - 1, на выходе получим
+// следующий массив:
+// 9 4 2
+// 2 2 6
+// 3 4 7
+
+
+
+
+void Print(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+            Console.Write($" {arr[i, j],3} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(from, to + 1);
+    return arr;
+}
+
+int[] MinElemenrArray(int[,] arr)
+{
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+    int min_num = arr[0, 0];
+    int[] index = new int[2];
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+            if (min_num > arr[i, j])
+            {
+                min_num = arr[i, j];
+                index[0] = i;
+                index[1] = j;
+            }
+    }
+    Console.WriteLine($"Mинимальный элемент в массиве: {arr[index[0], index[1]]}");
+    return index;
+}
+
+
+void WithoutRowColumn(int[,] arr, int[] m_index)
+{
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+    
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+            if (m_index[0] == i || m_index[1] == j) continue;
+            else Console.Write($"{arr[i, j],3}");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+Console.Write("Введите количество строк в массиве: ");
+int row_num = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов в массиве: ");
+int column_num = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите минимальное значение для диапозона случайного числа: ");
+int start = int.Parse(Console.ReadLine()!);
+Console.Write("Введите максимальное значение для диапозона случайного числа: ");
+int stop = int.Parse(Console.ReadLine()!);
+
+int[,] mass = MassNums(row_num, column_num, start, stop);
+Print(mass);
+
+int[] mass_new = MinElemenrArray(mass);
+System.Console.WriteLine();
+Console.WriteLine($"Получившийся массив:");
+WithoutRowColumn(mass, mass_new);
+
